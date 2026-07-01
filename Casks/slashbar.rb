96480ce6fb23444386aa2d357ca5db6ded465bf6
Harun Yardımcı <harun.yardimci@gmail.com>
2026-07-01 09:19:52 +0300

    slashbar 0.2.0

--- Casks/slashbar.rb
+++ Casks/slashbar.rb
@@ -1,6 +1,6 @@
 cask "slashbar" do
-  version "0.1.2"
-  sha256 "98ad14753599ae5b8c6274acd4c10ca9e363dfb27c16154c3409bbf5dea269d9"
+  version "0.2.0"
+  sha256 "9bdcb95870d1d745920a9f5564f2d8c7985c6e496edbafd93af345ee20753e9c"
 
   url "https://github.com/harun-yardimci/slashbar/releases/download/v#{version}/Slashbar.dmg",
       verified: "github.com/harun-yardimci/slashbar/"
